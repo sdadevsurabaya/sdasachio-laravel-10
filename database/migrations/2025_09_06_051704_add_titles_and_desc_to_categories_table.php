077a405646257b1827fe85ada91e665f44361f37
suryo <suryoatm@gmail.com>
2025-09-06 12:21:57 +0700

    add new field for categories

---
+++ database/migrations/2025_09_06_051704_add_titles_and_desc_to_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Jalankan migrasi.
+     */
+    public function up(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->string('ina_title')->nullable()->after('status');
+            $table->string('eng_title')->nullable()->after('ina_title');
+            $table->text('ina_desc')->nullable()->after('eng_title');
+            $table->text('eng_desc')->nullable()->after('ina_desc');
+            $table->integer('order')->nullable()->after('eng_desc'); // field angka
+        });
+    }
+
+    /**
+     * Rollback migrasi.
+     */
+    public function down(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn(['ina_title', 'eng_title', 'ina_desc', 'eng_desc']);
+        });
+    }
+};
